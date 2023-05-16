b136522895284be368eb9b3f8703e2d8a516d84f
edr3x <dhunganaannanta@gmail.com>
2023-05-16 17:47:58 +0545

    `init`

---
+++ lib/main_page.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:instcln/home_page.dart';
+
+import 'blank_page.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  final _pageViewController = PageController();
+  int _activePage = 0;
+
+  static final List<Widget> _widgetOptions = <Widget>[
+    const HomePage(),
+    const BlankPage(),
+    const BlankPage(),
+    const BlankPage(),
+    const BlankPage(),
+  ];
+
+  @override
+  void dispose() {
+    _pageViewController.dispose();
+    super.dispose();
+  }
+
+  static const List<IconData> _activeIcons = [
+    CupertinoIcons.home,
+    CupertinoIcons.search,
+    CupertinoIcons.add_circled,
+    CupertinoIcons.videocam_circle,
+    Icons.person_3_rounded
+  ];
+  static const List<IconData> _inactiveIcons = [
+    Icons.home_outlined,
+    Icons.search,
+    Icons.add_box_outlined,
+    Icons.video_library_outlined,
+    CupertinoIcons.person
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView(
+        controller: _pageViewController,
+        children: _widgetOptions,
+        onPageChanged: (index) {
+          setState(() {
+            _activePage = index;
+          });
+        },
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        currentIndex: _activePage,
+        onTap: (index) {
+          _pageViewController.animateToPage(index,
+              duration: const Duration(milliseconds: 200), curve: Curves.bounceOut);
+        },
+        items: List.generate(
+          _activeIcons.length,
+          (index) {
+            return BottomNavigationBarItem(
+              icon: Icon(
+                _inactiveIcons[index],
+              ),
+              activeIcon: Icon(
+                _activeIcons[index],
+              ),
+              label: '', // Remove label
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
